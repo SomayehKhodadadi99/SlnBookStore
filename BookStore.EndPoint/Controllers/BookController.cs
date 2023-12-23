711940bc77dea68a5da8dabc9ec46b17ed31d14a
MAC <khodadadi.somayeh99@gmail.com>
2023-12-23 13:27:58 +0330

    finish

--- BookStore.EndPoint/Controllers/BookController.cs
+++ BookStore.EndPoint/Controllers/BookController.cs
@@ -1,10 +1,15 @@
 ﻿using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Contexts;
 
 using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace BookStore.EndPoint.Controllers
 {
@@ -21,18 +26,69 @@ namespace BookStore.EndPoint.Controllers
 
         public IActionResult Index()
         {
-            List<Book> lstbooks = _db.Books.Include(p => p.Publisher).ToList();
-            return View(lstbooks);
+
+
+            //explicit Loading
+
+
+            ////SELECT[b].[Id], [b].[ISBN], [b].[InsertTime], [b].[IsRemoved], [b].[Price], [b].[PublisherId], [b].[RemoveTime], [b].[Title], [b].[UpdateTime]
+            ////FROM[Books] AS[b]
+            //    var objList = _db.Books.ToList();
+            //    foreach (var item in objList)
+            //    {
+
+
+            //        ////SELECT[p].[Id], [p].[InsertTime], [p].[IsRemoved], [p].[Location], [p].[Name], [p].[RemoveTime], [p].[UpdateTime]
+            //        ////FROM[Publishers] AS[p]
+            //        ////WHERE[p].[Id] = @__p_0
+
+            //        _db.Entry(item).Reference(x => x.Publisher).Load();
+            //        //SELECT[a].[AuthorId], [a].[BookId]
+            //        //FROM[AuthorBooks] AS[a]
+            //        //WHERE[a].[BookId] = @__p_0
+            //        _db.Entry(item).Collection(p=>p.AuthorBooks).Load();
+
+            //        //that will be autor is null then --->
+            //        foreach (var authorObj in  item.AuthorBooks)
+            //        {
+            //            _db.Entry(authorObj).Reference(p => p.Author).Load();
+            //        }
+            //}
+            //return View(objList);
+            //------------------------------------------------------------------------
+
+            //eager loading 
+
+            //SELECT[b].[Id], [b].[ISBN], [b].[InsertTime], [b].[IsRemoved], [b].[Price], [b].[PublisherId], [b].[RemoveTime], [b].[Title], [b].[UpdateTime], [p].[Id], [p].[InsertTime], [p].[IsRemoved], [p].[Location], [p].[Name], [p].[RemoveTime], [p].[UpdateTime]
+            //FROM[Books] AS[b]
+            //INNER JOIN[Publishers] AS[p] ON[b].[PublisherId] = [p].[Id]
+
+            //var objList = _db.Books.Include(p => p.Publisher).ToList();
+
+
+            //SELECT[b].[Id], [b].[ISBN], [b].[InsertTime], [b].[IsRemoved], [b].[Price], [b].[PublisherId], [b].[RemoveTime], [b].[Title], [b].[UpdateTime], [p].[Id], [p].[InsertTime], [p].[IsRemoved], [p].[Location], [p].[Name], [p].[RemoveTime], [p].[UpdateTime], [t].[AuthorId], [t].[BookId], [t].[Id], [t].[BirthDate], [t].[FirstName], [t].[InsertTime], [t].[IsRemoved], [t].[LastName], [t].[Location], [t].[RemoveTime], [t].[UpdateTime]
+            //FROM[Books] AS[b]
+            //INNER JOIN[Publishers] AS[p] ON[b].[PublisherId] = [p].[Id]
+            //LEFT JOIN(
+            //SELECT[a].[AuthorId], [a].[BookId], [a0].[Id], [a0].[BirthDate], [a0].[FirstName], [a0].[InsertTime], [a0].[IsRemoved], [a0].[LastName], [a0].[Location], [a0].[RemoveTime], [a0].[UpdateTime]
+            //FROM [AuthorBooks] AS [a]
+            //INNER JOIN [Authors] AS[a0] ON [a].[AuthorId] = [a0].[Id]
+            //) AS[t] ON[b].[Id] = [t].[BookId]
+            //ORDER BY[b].[Id], [p].[Id], [t].[AuthorId], [t].[BookId]
+
+
+            var objList = _db.Books.Include(p => p.Publisher).Include(p=>p.AuthorBooks).ThenInclude(p=>p.Author).ToList();
+            return View(objList);
+
+            //---------------------------------------------------------------------------
+
+
         }
         [HttpGet]
         public IActionResult Upsert(long? Id)
         {
             Bookvw book = new Bookvw();
-
-
             book.PublisherList = _db.Publishers.Select(p => new SelectListItem { Text = p.Name, Value = p.Id.ToString() }).ToList();
-
-
             if (Id == 0 || Id == null)
             {
                 //باید یه دراپ بسازم
@@ -41,10 +97,8 @@ namespace BookStore.EndPoint.Controllers
 
 
                 book.Book = new Book();
-
                 return View(book);
             }
-
             var bookResult = _db.Books.Where(x => x.Id == Id).SingleOrDefault();
             if (bookResult != null)
             {
@@ -52,7 +106,6 @@ namespace BookStore.EndPoint.Controllers
                 return View(book);
             }
             return NotFound();
-
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -68,8 +121,6 @@ namespace BookStore.EndPoint.Controllers
 
                     Book bookProxy = new Book();
                     var result = await _db.Books.AddAsync(book);
-
-
                 }
                 else
                 {
@@ -224,6 +275,27 @@ namespace BookStore.EndPoint.Controllers
 
             _db.SaveChanges();
             return RedirectToAction(nameof(ManageAuthors), new { @id = bookId });
+        }
+
+   
+        public async Task<IActionResult> Delete(long Id)
+        {
+
+            Book myBook = new();
+             myBook=_db.Books.FirstOrDefault(x => x.Id == Id);
+
+            if (myBook!=null)
+            {
+                _db.Books.Remove(myBook);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            { 
+                return NotFound();
+            }
+
+            
         }
 
 
